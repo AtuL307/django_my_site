f8cecafe41d5ac84ee5a82a7c8f3e10e1849dddd
AtuL307 <vaibhavpawar021@gmail.com>
2024-03-16 20:56:22 +0530

    update new

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('posts', views.blog, name='posts'),
-    #path('posts/<slug>'),
+    path('posts/', views.blog_posts, name='posts-page'),
+    path('posts/<slug:slug>/', views.post_details, name='postDetail-page'),
     
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
-def blog(req):
+def blog_posts(req):
     return HttpResponse('<h1>Blog</h1>')
+
+def post_details(req):
+   pass
